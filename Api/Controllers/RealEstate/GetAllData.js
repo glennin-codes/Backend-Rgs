b98de84f328215d324c25de089991912c25e7947
ayiendaglen <glen.nyandiri@students.kyu.ac.ke>
2023-09-21 13:22:37 +0300

    added search query

--- Api/Controllers/RealEstate/GetAllData.js
+++ Api/Controllers/RealEstate/GetAllData.js
@@ -5,16 +5,26 @@ export const getData = async (req, res) => {
   const limit = parseInt(req.query.limit) || 10;
   const startDate = req.query.startDate;
   const endDate = req.query.endDate;
+  const searchQuery = req.query.search || ''; // Get the search query from the request
 
   try {
-    let query = {}; // Initialize an empty query object
+    let query = {};
 
     // Check if startDate and endDate parameters are provided
     if (startDate && endDate) {
       query.dateField = {
-        $gte: new Date(startDate), // Greater than or equal to start date
-        $lte: new Date(endDate),   // Less than or equal to end date
+        $gte: new Date(startDate),
+        $lte: new Date(endDate),
       };
+    }
+
+    // Build a query to search by name, location, and paymentId
+    if (searchQuery) {
+      query.$or = [
+        { name: { $regex: searchQuery, $options: 'i' } }, // Case-insensitive search by name
+        { location: { $regex: searchQuery, $options: 'i' } }, // Case-insensitive search by location
+        { paymentId: { $regex: searchQuery, $options: 'i' } }, // Case-insensitive search by paymentId
+      ];
     }
 
     const skip = (page - 1) * limit;
