ffa3ccf923a6e08cc3dcd9eac663d3a78df40c84
ayiendaglen <glen.nyandiri@students.kyu.ac.ke>
2023-10-04 20:43:21 +0300

    added some checks

--- Api/Controllers/User/Employee/UpdateEmployee.js
+++ Api/Controllers/User/Employee/UpdateEmployee.js
@@ -1,11 +1,24 @@
 import User from "../../../Models/User.js";
 import bcrypt from "bcrypt";
+
 export const UpdateEmployee = async (req, res) => {
   try {
     const id = req.params.id;
     const password = req.body.password;
 
     const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Check if the updated location already exists for another user
+    const existingUserWithLocation = await User.findOne({
+      location: req.body.location,
+      _id: { $ne: id }, // Exclude the current user being updated
+    });
+
+    if (existingUserWithLocation) {
+      return res
+        .status(409)
+        .json({ message: "Location already exists for another user" });
+    }
 
     const employee = await User.findOneAndUpdate(
       { _id: id },
@@ -14,16 +27,15 @@ export const UpdateEmployee = async (req, res) => {
         location: req.body.location,
         phone: req.body.phone,
         email: req.body.email,
-
-        // photo: "",
         password: hashedPassword,
       },
       { new: true }
     );
+
     if (employee) {
-      return res.status(200).json({ message: "user updated successfully" });
+      return res.status(200).json({ message: "User updated successfully" });
     } else {
-      return res.status(404).json({ message: "user not found" });
+      return res.status(404).json({ message: "User not found" });
     }
   } catch (error) {
     console.error(error);
