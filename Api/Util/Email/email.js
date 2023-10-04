8cbe7ab14f6299ee33dc23e4030b68584194f8f3
ayiendaglen <glen.nyandiri@students.kyu.ac.ke>
2023-10-04 19:04:40 +0300

    testing

--- Api/Util/Email/email.js
+++ Api/Util/Email/email.js
@@ -1,27 +1,24 @@
-import sgMail from '@sendgrid/mail';
-import { sendCredentials } from './Sendmailgen.js';
+import sgMail from "@sendgrid/mail";
+import { sendCredentials } from "./Sendmailgen.js";
 import dotenv from "dotenv";
 
 dotenv.config();
-export const sendEmail=async(name,email,pwd)=>{
-  
-console.log(name,email,pwd);
-    const msg = {
-        to: email,
-        from: 'ayiendaglen@gmail.com',
-        subject: 'Welcome to Rgs',
-        html: sendCredentials(name,email,pwd),
-      };
-   
-      try {
-        sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-       const result= await sgMail.send(msg);
-       if(result){
-        console.log(result);
-       }
+export const sendEmail = async (name, email, pwd) => {
+  console.log(name, email, pwd);
+  const msg = {
+    to: email,
+    from: "ayiendaglen@gmail.com",
+    subject: "Welcome to Rgs",
+    html: sendCredentials(name, email, pwd),
+  };
 
-      } catch (error) {
-        console.error(error);
-
-      }
-}
+  try {
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    const result = await sgMail.send(msg);
+    if (result) {
+      console.log(result);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
