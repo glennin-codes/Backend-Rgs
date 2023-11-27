f34d02b0c9f0ac33cad0d8d11d53f015e03d9cb3
ayiendaglen <glen.nyandiri@students.kyu.ac.ke>
2023-11-27 16:03:38 +0300

    added an error catcher

--- Api/Controllers/RealEstate/GetAllData.js
+++ Api/Controllers/RealEstate/GetAllData.js
@@ -39,7 +39,11 @@ export const getData = async (req, res) => {
       .skip(skip)
       .limit(limit);
 
-      res.status(200).json({ items, totalItems }); 
+      if (items.length > 0) {
+        res.status(200).json({ items, totalItems, message: "Fetched successfully" });
+      } else {
+        res.status(404).json({ message: "No items found" });
+      }
   } catch (err) {
     res.status(500).json({ message: 'Internal Server Error' });
   }
