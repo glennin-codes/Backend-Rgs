be368179b55410f0ff5a64f692f16051b5928897
ayiendaglen <glen.nyandiri@students.kyu.ac.ke>
2023-12-17 16:13:27 +0300

    maximum request body size

--- Api/app.js
+++ Api/app.js
@@ -9,8 +9,8 @@ import indexRouter from './routes/index.js';
 const app = express();
 app.use(cors());
 app.use(logger('dev'));
-app.use(json({limit: '50mb'}));
-app.use(urlencoded({limit: '50mb',extended: true,parameterLimit:50000}));
+app.use(express.json({limit: '60mb'}));
+app.use(express.urlencoded({limit: '60mb',extended: true,parameterLimit:50000}));
 app.use(cookieParser());
 // app.use(static(join(__dirname, 'public')));
 
